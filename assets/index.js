3c39f86301ad19e663eb16be21e19c9b2734e157
Summer Jade <summerjadefrosal@gmail.com>
2022-07-10 19:53:18 -0400

    added on click event listener and list group item

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,31 @@
-// When I open the weather dashboard
-// Then I am presented with the kast searched city forecast
+
+$("#searchBtn").on("click", function(event) {
+    event.preventDefault();
+
+    let city = $("#enterCity").val().trim();
+    currentCondition(city);
+    if (!searchHistoryList.includes(city)) {
+        searchHistoryList.push(city);
+        let searchedCity = $(`
+            <li class="list-group-item">${city}</li>
+        `);
+        $("searchHistory").append(searchedCity);
+    };
+
+    localStorage.setItem("city", JSON.stringify(searchHistoryList));
+    console.log(searchHistoryList);
+    });
+
+//WHEN I click on a city in the search history
+//THEN I am again presented with current and future conditions for that city
+
+$(document).on("click", "list-group-item", function() {
+    let listCity = $(this).text();
+    currentCondition(listCity);
+});
+
+// WHEN I open the weather dashboard
+// THEN I am presented with the last searched city forecast
 $(document).ready(function() {
     let searchHistoryArr = JSON.parse(localStorage.getItem("city"));
 
@@ -10,3 +36,4 @@ $(document).ready(function() {
         console.log('Last Searched city: ${lastSearchedCity');
     }
 });
+
